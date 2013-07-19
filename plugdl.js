d1084b6fa23ef09bdf716c618c142151a3519617
Bftech <corentin.monmasson@gmail.com>
2013-07-19 23:54:05 +0200

    try a rly bad other way to do that

--- plugdl.js
+++ plugdl.js
@@ -1,11 +1,12 @@
 javascript: (
 function () {
-  alert('test1');
-  var ytframe = document.getElementById('yt-frame').contentDocument;
-  var yto = ytframe.getElementById('ytp');
-  alert('test2');
-  var ytPlugAdd = yto.getAttribut('data').split('?')[0];
-  alert(ytPlugAdd);
-  alert('test3');
+  //var ytframe = document.getElementById('yt-frame').contentDocument;
+  //var yto = ytframe.getElementById('ytp');
+  //var ytPlugAdd = yto.getAttribut('data').split('?')[0];
+  //alert(ytPlugAdd);
+  
+  var bodyCode = document.body.innerHTML;
+  var ytUrl = bodycode.split('ytp')[1].substring(7,36);
+  alert(ytUrl);
 })();
 
