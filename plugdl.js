96c1f0b7a8a011aada71b269ce9f701079f024e5
Bftech <corentin.monmasson@gmail.com>
2013-07-20 01:27:21 +0200

    light release

--- plugdl.js
+++ plugdl.js
@@ -1,12 +1,11 @@
 javascript: (
 function () {
-  //var vidUrl = '';
+if (windows.name.indexOf('plug.dj') != -1){
   if ('1' === Playback.media.format) { // youtube
-        //vidURL = 'http://www.youtube-mp3.org/?c#v=' + Playback.media.cid;
-        //var append = '<p><a href="' + vidURL + '" target="_blank" id="plugdl-btn-dl" style="color:#FFF">Download</a></p>';
-        //document.getElementById('meta-frame').appendChild(append);
-        alert(vidURL);
         window.open('http://www.youtube-mp3.org/?c#v=' + Playback.media.cid);
   }
+}else{
+  alert('You are not on plug.dj')
+}
 })();
 
