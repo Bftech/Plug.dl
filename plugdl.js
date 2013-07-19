d031779f8fec665ec626c379750c94660c17e16a
Bftech <corentin.monmasson@gmail.com>
2013-07-19 23:12:33 +0200

    test

--- plugdl.js
+++ plugdl.js
@@ -1,10 +1,12 @@
 javascript: (
 function () {
+  alert('test1');
   var ytframe = document.getElementById('yt-frame');
-  ytframe = ytframe.contentDocument || frame.document;
+  ytframe = ytframe.contentDocument || ytframe.document;
   var yto = ytframe.getElementById('ytp');
   
   var ytPlugAdd = yto.getAttribut('data').split('?')[0];
   alert(ytPlugAdd);
+  alert('test2');
 })();
 
