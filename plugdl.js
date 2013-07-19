5f393ddbe9d4c4533364968ff353bcb8feefc07b
Bftech <corentin.monmasson@gmail.com>
2013-07-20 01:18:04 +0200

    Update plugdl.js

--- plugdl.js
+++ plugdl.js
@@ -5,8 +5,8 @@ function () {
         vidURL = 'http://www.youtube-mp3.org/?c#v=' + Playback.media.cid;
         //var append = '<p><a href="' + vidURL + '" target="_blank" id="plugdl-btn-dl" style="color:#FFF">Download</a></p>';
         //document.getElementById('meta-frame').appendChild(append);
+        alert(vidURL);
         window.open(vidUrl);
   }
-  //alert(vidURL);
 })();
 
