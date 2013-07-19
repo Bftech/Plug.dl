2c6987ca437e2f6fded7d6b1d12344b8de5b410e
Bftech <corentin.monmasson@gmail.com>
2013-07-20 01:14:14 +0200

    Update plugdl.js

--- plugdl.js
+++ plugdl.js
@@ -1,14 +1,11 @@
 javascript: (
 function () {
-  //var ytframe = document.getElementById('yt-frame').contentDocument;
-  //var yto = ytframe.getElementById('ytp');
-  //var ytPlugAdd = yto.getAttribut('data').split('?')[0];
-  //alert(ytPlugAdd);
   var vidUrl = '';
-  alert('test');
   if ('1' === Playback.media.format) { // youtube
-        vidURL = 'http://www.youtube.com/watch?v=' + Playback.media.cid;
+        vidURL = 'http://www.youtube-mp3.org/?c#v=' + Playback.media.cid;
+        var append = '<p><a href="' + vidURL + '" target="_blank" id="plugdl-btn-dl" style="color:#FFF">Download</a></p>';
+        document.getElementById('meta-frame').appendChild(append);
   }
-  alert(vidURL);
+  //alert(vidURL);
 })();
 
