923bbf6c04fc9387114c5cca3ba7c376e996b615
Bftech <corentin.monmasson@gmail.com>
2013-07-19 23:22:04 +0200

    Update plugdl.js

--- plugdl.js
+++ plugdl.js
@@ -1,12 +1,11 @@
 javascript: (
 function () {
   alert('test1');
-  var ytframe = document.getElementById('yt-frame');
-  ytframe = ytframe.contentDocument || ytframe.document;
+  var ytframe = document.getElementById('yt-frame').contentDocument;
   var yto = ytframe.getElementById('ytp');
-  
+  alert('test2');
   var ytPlugAdd = yto.getAttribut('data').split('?')[0];
   alert(ytPlugAdd);
-  alert('test2');
+  alert('test3');
 })();
 
